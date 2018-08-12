a1ca854fae31a977bd99169a3c4321c4649fe686
sajib <sajib@github.com>
2018-08-12 23:56:33 +0600

    firs twebsite

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    
+    // skdslider js start 
+    jQuery('.skdslider').skdslider({delay:5000, animationSpeed: 2000,showNextPrev:true,showPlayButton:false,autoSlide:true,animationType:'fading'});
+ 
+    
+    
+});
+
+    
